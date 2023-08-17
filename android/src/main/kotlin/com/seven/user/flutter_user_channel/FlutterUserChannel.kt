7be337b3fc11c6da8f0b83b11569e52c9a2fc81f
wangqi <a80741@gree.com.cn>
2023-08-17 14:12:52 +0800

    0.0.6

--- android/src/main/kotlin/com/seven/user/flutter_user_channel/FlutterUserChannel.kt
+++ android/src/main/kotlin/com/seven/user/flutter_user_channel/FlutterUserChannel.kt
@@ -7,6 +7,7 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.embedding.engine.FlutterEngine
 
 /** FlutterUserChannelPlugin */
 class FlutterUserChannel {
