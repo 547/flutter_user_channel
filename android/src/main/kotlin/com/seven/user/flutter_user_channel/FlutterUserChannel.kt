638cfd2066f27e4de999cca3c2cc93a54cc5e480
wangqi <a80741@gree.com.cn>
2023-08-17 15:21:05 +0800

    0.0.8

--- android/src/main/kotlin/com/seven/user/flutter_user_channel/FlutterUserChannel.kt
+++ android/src/main/kotlin/com/seven/user/flutter_user_channel/FlutterUserChannel.kt
@@ -11,16 +11,18 @@ import io.flutter.embedding.engine.FlutterEngine
 
 /** FlutterUserChannelPlugin */
 class FlutterUserChannel {
-   companion object fun updateUserToken(token: String, engine: FlutterEngine) {
-        val messenger = engine.dartExecutor.binaryMessenger
+   companion object {
+       fun updateUserToken(token: String, engine: FlutterEngine) {
+           val messenger = engine.dartExecutor.binaryMessenger
 
-        // 新建一个 Channel 对象
-        val channel = MethodChannel(messenger, "com.seven.user.flutter_user_channel")
+           // 新建一个 Channel 对象
+           val channel = MethodChannel(messenger, "com.seven.user.flutter_user_channel")
 
-        // 调用 Flutter 中的方法
-        val params = mapOf(
-            "token" to token
-        )
-        channel.invokeMethod("updateWithUserToken", params)
-  }
+           // 调用 Flutter 中的方法
+           val params = mapOf(
+               "token" to token
+           )
+           channel.invokeMethod("updateWithUserToken", params)
+       }
+   }
 }
