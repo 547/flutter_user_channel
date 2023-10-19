162dc2c489476eced4569858e464a628b6903ab0
wangqi <timer_sevenwang@163.com>
2023-10-19 15:12:25 +0800

    修改 FlutterUserChannel改写成一个单例

--- ios/Classes/FlutterUserChannel.swift
+++ ios/Classes/FlutterUserChannel.swift
@@ -2,23 +2,56 @@ import Flutter
 import UIKit
 
 public class FlutterUserChannel: NSObject {
-  public static func updateUserToken(with token: String?, engine: FlutterEngine, callback: @escaping (Any?) -> ()) {
-        // 创建渠道
-        let channel = FlutterMethodChannel(name: "com.seven.user.flutter_user_channel", binaryMessenger: engine.binaryMessenger)
-        // 通过渠道调用 Flutter 的方法
-        let params: Dictionary<String, String> = ["token": token ?? ""]
-        channel.invokeMethod("updateWithUserToken", arguments: params) { result in
-            callback(result)
+    public static let shared = FlutterUserChannel()
+    public static let methodChannelName = "com.seven.user.flutter_user_channel"
+    private var engine: FlutterEngine?
+    private var channel: FlutterMethodChannel?
+    private var binaryMessenger: FlutterBinaryMessenger?
+    /// 是否已经启动
+    public var isSetup: Bool {
+        var result = false
+        if channel != nil, binaryMessenger != nil {
+            result = true
         }
-  }
-  /// 传递其他信息
-  public static func transmitOtherInfo(with info: [String : Any], engine: FlutterEngine, callback: @escaping (Any?) -> ()) {
+        return result
+    }
+    private override init() {
+        super.init()
+    }
+    /// 通过已有的 FlutterEngine 启动
+    public static func setupWithEngine(engine: FlutterEngine) -> FlutterUserChannel {
+        let flutterUserChannel = FlutterUserChannel.shared
+        flutterUserChannel.engine = engine
+        flutterUserChannel.binaryMessenger = engine.binaryMessenger
         // 创建渠道
-        let channel = FlutterMethodChannel(name: "com.seven.user.flutter_user_channel", binaryMessenger: engine.binaryMessenger)
-        // 通过渠道调用 Flutter 的方法
-        let params = ["info": info]
-        channel.invokeMethod("transmitOtherInfo", arguments: params) { result in
-            callback(result)
+        flutterUserChannel.channel = FlutterMethodChannel(name: methodChannelName, binaryMessenger: engine.binaryMessenger)
+        return flutterUserChannel
+    }
+    public static func updateUserToken(with token: String?, engine: FlutterEngine, callback: @escaping (Any?) -> ()) {
+        let flutterUserChannel = FlutterUserChannel.shared
+        if !flutterUserChannel.isSetup {
+            setupWithEngine(engine: engine)
         }
-  }
+        if let channel = flutterUserChannel.channel {
+            // 通过渠道调用 Flutter 的方法
+            let params: Dictionary<String, String> = ["token": token ?? ""]
+            channel.invokeMethod("updateWithUserToken", arguments: params) { result in
+                callback(result)
+            }
+        }
+    }
+    /// 传递其他信息
+    public static func transmitOtherInfo(with info: [String : Any], engine: FlutterEngine, callback: @escaping (Any?) -> ()) {
+        let flutterUserChannel = FlutterUserChannel.shared
+        if !flutterUserChannel.isSetup {
+            setupWithEngine(engine: engine)
+        }
+        if let channel = flutterUserChannel.channel {
+            // 通过渠道调用 Flutter 的方法
+            let params = ["info": info]
+            channel.invokeMethod("transmitOtherInfo", arguments: params) { result in
+                callback(result)
+            }
+        }
+    }
 }
