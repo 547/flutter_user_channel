a080b55383d23fa08f7b098b0fcf8e54443ce713
wangqi <a80741@gree.com.cn>
2023-10-19 13:35:43 +0800

    修改 MethodChannelFlutterUserChannel

--- lib/flutter_user_channel_method_channel.dart
+++ lib/flutter_user_channel_method_channel.dart
@@ -18,18 +18,27 @@ class MethodChannelFlutterUserChannel extends FlutterUserChannelPlatform {
     methodChannel.setMethodCallHandler((call) async {
       Logger.log(
           "Native 调用 Flutter 成功，channel: ${methodChannel.name} 方法：${call.method} 参数是：${call.arguments}");
+      dynamic result;
       switch (call.method) {
         case "updateWithUserToken":
           final token = call.arguments["token"];
-          final result = await updateUserToken.call(token);
-          return result;
+          result = await updateUserToken.call(token);
+          break;
         case "transmitOtherInfo":
           final info = call.arguments["info"];
-          final result = await transmitOtherInfo.call(info);
-          return result;
+          await transmitOtherInfo.call(info).then((value) {
+            result = value;
+            Logger.log("===========transmitOtherInfo then $value");
+          }).catchError((onError) {
+            result = onError;
+            Logger.log("===========transmitOtherInfo catchError $onError");
+          });
+          break;
         default:
-          return FlutterError("方法名错误");
+          result = FlutterError("方法名错误");
+          break;
       }
+      return result;
     });
   }
 
