6ca0b3aecf696433e07d1d9f9f194d59aea69b4c
wangqi <a80741@gree.com.cn>
2023-10-19 10:26:41 +0800

    1.0.0

--- ios/Classes/FlutterUserChannel.swift
+++ ios/Classes/FlutterUserChannel.swift
@@ -12,7 +12,7 @@ public class FlutterUserChannel: NSObject {
         }
   }
   /// 传递其他信息
-  public static func transmitOtherInfo(with info: [String : Any]?, engine: FlutterEngine, callback: @escaping (Any?) -> ()) {
+  public static func transmitOtherInfo(with info: [String : Any], engine: FlutterEngine, callback: @escaping (Any?) -> ()) {
         // 创建渠道
         let channel = FlutterMethodChannel(name: "com.seven.user.flutter_user_channel", binaryMessenger: engine.binaryMessenger)
         // 通过渠道调用 Flutter 的方法
