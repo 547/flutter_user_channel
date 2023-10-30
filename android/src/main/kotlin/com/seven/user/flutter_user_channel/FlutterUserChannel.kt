95ac6baaa6c4b900130ccc35fe50bf858a760d65
wangqi <timer_sevenwang@163.com>
2023-10-30 13:55:58 +0800

    修改 Android FlutterUserChannel改写成单例，避免多次创建FlutterMethodChannel

--- android/src/main/kotlin/com/seven/user/flutter_user_channel/FlutterUserChannel.kt
+++ android/src/main/kotlin/com/seven/user/flutter_user_channel/FlutterUserChannel.kt
@@ -11,30 +11,55 @@ import io.flutter.embedding.engine.FlutterEngine
 
 /** FlutterUserChannelPlugin */
 class FlutterUserChannel {
+    private var tokenChannel: MethodChannel? = null
+    private var otherInfoChannel: MethodChannel? = null
    companion object {
-       fun updateUserToken(token: String, engine: FlutterEngine) {
-           val messenger = engine.dartExecutor.binaryMessenger
+       private var instance: FlutterUserChannel? = null
+       fun getInstance(): FlutterUserChannel {
+           if (instance == null) {
+               synchronized(FlutterUserChannel::class) {
+                   if (instance == null) {
+                       instance = FlutterUserChannel()
+                   }
+               }
+           }
+           return instance!!
+       }
 
-           // 新建一个 Channel 对象
-           val channel = MethodChannel(messenger, "com.seven.user.flutter_user_channel.token")
+       private const val tokenChannelName = "com.seven.user.flutter_user_channel.token"
+       private const val otherInfoChannelName = "com.seven.user.flutter_user_channel.other_info"
+       private const val tokenChannelMethod = "updateWithUserToken"
+       private const val otherInfoChannelMethod = "transmitOtherInfo"
+
+
+       fun updateUserToken(token: String, engine: FlutterEngine) {
+           if (instance?.tokenChannel == null) {
+               val messenger = engine.dartExecutor.binaryMessenger
+               // 新建一个 Channel 对象
+               instance?.tokenChannel = MethodChannel(messenger, tokenChannelName)
+           }
+
 
            // 调用 Flutter 中的方法
            val params = mapOf(
                "token" to token
            )
-           channel.invokeMethod("updateWithUserToken", params)
+           instance?.tokenChannel?.invokeMethod(tokenChannelMethod, params)
        }
        fun transmitOtherInfo(info: Map<String, Any>, engine: FlutterEngine) {
-           val messenger = engine.dartExecutor.binaryMessenger
+           if (instance?.otherInfoChannel == null) {
+               val messenger = engine.dartExecutor.binaryMessenger
 
-           // 新建一个 Channel 对象
-           val channel = MethodChannel(messenger, "com.seven.user.flutter_user_channel.other_info")
+               // 新建一个 Channel 对象
+               instance?.otherInfoChannel = MethodChannel(messenger, otherInfoChannelName)
+           }
+
 
            // 调用 Flutter 中的方法
            val params = mapOf(
                "info" to info
            )
-           channel.invokeMethod("transmitOtherInfo", params)
+           instance?.otherInfoChannel?.invokeMethod(otherInfoChannelMethod, params)
        }
    }
 }
